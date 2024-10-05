a1c479e021724108c179a9f987e969549cf26684
ductho05 <ductho23pro@gmail.com>
2024-10-05 10:07:57 +0700

    fix api https

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 const isDeploy = true;
 
 export const API_URL = isDeploy
-  ? "http://103.231.249.242:8080"
+  ? "//103.231.249.242:8080"
   : "http://localhost:8080";
 export const NAME_SHOP = "Nail shop";
 export const ADMIN_NAME = "Nail Shop Admin"
